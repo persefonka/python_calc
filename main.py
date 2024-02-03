ba8a2e189bce73ee8fdd41804c45b942886f268d
persefonka <i-goole@ya.ru>
2024-02-03 18:55:39 +0400

    загрузка первых файлов

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+def inputt():
+    num1=int(input())
+    num2=int(input())
+    choice=input()
+    return num1, num2, choice
+def calc(num1, num2, choice):
+    if choice=="+":
+        return num1+num2
+    elif choice=="-":
+        return num1-num2
+    else:
+        return 0
+def start():
+    x,y,z=inputt()
+    print(calc(x,y,z))
+start()
+
 
